1cd1bcf01d73cf08d69c0321dd6cf61689ccbde6
Emad Albarnawi <Emadk789@hotmail.com>
2020-11-05 20:49:12 +0300

    refactor: main.storyboard and delete ImageViewController.

---
+++ PrismVision/Extensions/UIImage + Extra.swift
@@ -0,0 +1,38 @@
+//
+//  UIImage + Extra.swift
+//  PrismVision
+//
+//  Created by Emad Albarnawi on 05/11/2020.
+//
+
+import UIKit;
+
+extension UIImage {
+    func crop( rect: CGRect) -> UIImage {
+        var rect = rect
+        rect.origin.x*=self.scale
+        rect.origin.y*=self.scale
+        rect.size.width*=self.scale
+        rect.size.height*=self.scale
+
+        let imageRef = self.cgImage!.cropping(to: rect)
+        let image = UIImage(cgImage: imageRef!, scale: self.scale, orientation: self.imageOrientation)
+        return image
+    }
+    
+    func resizeImage(targetSize: CGSize) -> UIImage {
+        let size = self.size
+        let widthRatio  = targetSize.width  / size.width // 0.35
+        let heightRatio = targetSize.height / size.height // 0.42
+        let newSize = widthRatio > heightRatio ?  CGSize(width: size.width * heightRatio, height: size.height * heightRatio) : CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        self.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
+      }
+    
+}
