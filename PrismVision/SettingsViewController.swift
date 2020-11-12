3241e2d710a781b780ec94f653f8e268ffef2bba
Emad Albarnawi <Emadk789@hotmail.com>
2020-11-12 22:36:14 +0300

    feat: Redirect the user to the settings app to change the language as recommended by Apple.

--- PrismVision/SettingsViewController.swift
+++ PrismVision/SettingsViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var titleLabel: UILabel!
+//    @IBOutlet weak var detailLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,36 +23,60 @@ class SettingsViewController: UIViewController {
 
 extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        2
+//        2
+        1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let language = NSLocale.autoupdatingCurrent.languageCode;
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!;
 //        if indexPath.row == 0;
-        cell.textLabel?.text = indexPath.row == 0 ? "English" : "العربية";
+        //TODO: Add it to the Localizable.strings file
+        cell.textLabel?.text = NSLocalizedString("Language", comment: "The Language of the app");
         
-        switch indexPath.row {
-        case 0:
-            //TODO: Add this to the userDefaults!
-            cell.accessoryType = .checkmark;
+        switch language {
+        case "en":
+//            cell.textLabel?.text = "English";
+            cell.detailTextLabel?.text = "English";
+//            cell.rightDetail.text
+//            cell.accessoryType = .checkmark;
+        case "ar":
+            cell.detailTextLabel?.text = "العربية";
+//            cell.textLabel?.text = "العربية";
+//            cell.accessoryType = .checkmark;
         default:
             break;
         }
+//        cell.textLabel?.text = indexPath.row == 0 ? "English" : "العربية";
+        
+//        switch indexPath.row {
+//        case 0:
+//            //TODO: Add this to the userDefaults!
+//            cell.accessoryType = .checkmark;
+//        default:
+//            break;
+//        }
         cell.selectionStyle = .none;
         return cell;
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let i = indexPath;
-        let myIndexPath = IndexPath(row: 0, section: 1);
-        let cell = tableView.cellForRow(at: indexPath)!;
-        let englishCell = tableView.cellForRow(at: IndexPath(row: 0, section: 0));
-        let arabicCell = tableView.cellForRow(at: IndexPath(row: 1, section: 0));
-
-        englishCell?.accessoryType = .none;
-        arabicCell?.accessoryType = .none;
         
-        cell.accessoryType = .checkmark;
-        cell.isSelected = false;
+        
+//        Locale.autoupdatingcurrent.languageCode
+        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+//        let i = indexPath;
+//        let myIndexPath = IndexPath(row: 0, section: 1);
+//        let cell = tableView.cellForRow(at: indexPath)!;
+//        let englishCell = tableView.cellForRow(at: IndexPath(row: 0, section: 0));
+//        let arabicCell = tableView.cellForRow(at: IndexPath(row: 1, section: 0));
+//
+//        englishCell?.accessoryType = .none;
+//        arabicCell?.accessoryType = .none;
+//
+//        cell.accessoryType = .checkmark;
+//        cell.isSelected = false;
+        
     }
     
 }
