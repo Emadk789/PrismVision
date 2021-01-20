893f7c7a363202a9f8da6ca83b3f4da4492cb3d8
Emad Albarnawi <Emadk789@hotmail.com>
2021-01-20 04:31:22 +0300

    feat: Control the collection view from the controls buttons, i.e., Next and Previous. This is also reflected on the pageControl.

--- PrismVision/View Controller/TutorialViewController.swift
+++ PrismVision/View Controller/TutorialViewController.swift
@@ -38,12 +38,27 @@ class TutorialViewController: UIViewController {
         pageControl.translatesAutoresizingMaskIntoConstraints = false
         return pageControl
     }()
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "house")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test Label"
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray
+        previousButton.isHidden = true
         addSubViews()
+        addActions()
         configurCollectionView()
         configurViewsAnchors()
     }
@@ -52,6 +67,18 @@ class TutorialViewController: UIViewController {
         view.addSubview(pageControl)
         view.addSubview(nextButton)
         view.addSubview(previousButton)
+        view.addSubview(imageView)
+        view.addSubview(descriptionLabel)
+    }
+    private func addActions() {
+        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        previousButton.addTarget(self, action: #selector(previousButtonClicked), for: .touchUpInside)
+    }
+    @objc private func nextButtonClicked() {
+        manageContols(.next)
+    }
+    @objc private func previousButtonClicked() {
+        manageContols(.previous)
     }
 
     private func configurCollectionView() {
@@ -65,6 +92,8 @@ class TutorialViewController: UIViewController {
         configurNextButtonAnchors()
         configurPreviousButtonAnchors()
         configurPageControlAnchors()
+        configurImageViewAnchors()
+        configurDescriptionLabelAnchors()
     }
     private func configurCollectionViewAnchors() {
         collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
@@ -85,6 +114,50 @@ class TutorialViewController: UIViewController {
         pageControl.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8).isActive = true
     }
+    private func configurImageViewAnchors() {
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100).isActive = true
+        imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4).isActive = true
+    }
+    private func configurDescriptionLabelAnchors() {
+        descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+    }
+    // MARK:- Helper(s)
+
+    enum Controls {
+        case next, previous
+    }
+    private func manageContols(_ control: Controls) {
+        let indexPaths = collectionView.indexPathsForVisibleItems
+        let indexPath = indexPaths[0]
+        var newIndexPath: IndexPath
+        
+        switch control {
+        case .next:
+            newIndexPath = IndexPath(item: indexPath.item + 1, section: 0)
+        case .previous:
+            newIndexPath = IndexPath(item: indexPath.item - 1, section: 0)
+        }
+        collectionView.scrollToItem(at: newIndexPath, at: .bottom, animated: true)
+        updateContolsVisibility(for: newIndexPath)
+    }
+    private func updateContolsVisibility(for indexPath: IndexPath) {
+        pageControl.currentPage = indexPath.item
+        if indexPath.item == 0 {
+            previousButton.isHidden = true
+        }
+        if indexPath.item != 0 {
+            previousButton.isHidden = false
+            nextButton.isHidden = false
+        }
+        if indexPath.item == collectionView.numberOfItems(inSection: 0) - 1 {
+            nextButton.isHidden = true
+            //TODO: Show Done Button! or Change the title of the next button
+        }
+    }
 }
 
 class TutorialCollectionViewCell: UICollectionViewCell {
@@ -101,13 +174,23 @@ extension TutorialViewController: UICollectionViewDelegate, UICollectionViewData
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         cell.backgroundColor = .systemGray3
+        if indexPath.item == 0 {
+            cell.backgroundColor = .systemBlue
+        }
+        if indexPath.item == 1 {
+            cell.backgroundColor = .systemPurple
+        }
+        if indexPath.item == 2 {
+            cell.backgroundColor = .systemPink
+        }
+        
         return cell
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         for cell in collectionView.visibleCells {
             if let indexPath = collectionView.indexPath(for: cell) {
-                pageControl.currentPage = indexPath.item
+                updateContolsVisibility(for: indexPath)
             }
         }
     }
