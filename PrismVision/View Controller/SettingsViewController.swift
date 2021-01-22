d76a009e5dd12dd0da3de18f33c245a89bcacf07
Emad Albarnawi <Emadk789@hotmail.com>
2021-01-23 01:25:11 +0300

    feat: Finish the Arabic version of the tutorial, add it as a cell in the settings page. refactor: SettingsViewController

--- PrismVision/View Controller/SettingsViewController.swift
+++ PrismVision/View Controller/SettingsViewController.swift
@@ -16,10 +16,14 @@ class SettingsViewController: UIViewController {
         tableView.delegate = self;
         tableView.dataSource = self;
         tableView.separatorStyle = .none;
+        
     }
 }
 
 extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         1
     }
@@ -29,21 +33,39 @@ extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!;
 
-        cell.textLabel?.text = NSLocalizedString("Language", comment: "The Language of the app");
         
-        switch language {
-        case "en":
-            cell.detailTextLabel?.text = "English";
-        case "ar":
-            cell.detailTextLabel?.text = "العربية";
+        switch indexPath.section {
+        case 0:
+            cell.imageView?.image = UIImage(systemName: "globe")
+            cell.imageView?.tintColor = .systemGray
+            cell.textLabel?.text = NSLocalizedString("Language", comment: "The Language of the app");
+            cell.detailTextLabel?.text = NSLocalizedString("English", comment: "Language")
+            
+        case 1:
+            cell.imageView?.image = UIImage(systemName: "questionmark.circle")
+            cell.imageView?.tintColor = .systemGray
+            cell.detailTextLabel?.text = ""
+            cell.textLabel?.text = NSLocalizedString("Tutorial", comment: "Tutorial")
+            break
         default:
-            break;
+            break
         }
+        
         cell.selectionStyle = .none;
         return cell;
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
         UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        }
+        else if indexPath.section == 1 {
+            let vc = TutorialViewController()
+            present(vc, animated: true, completion: nil)
+        }
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
     
 }
